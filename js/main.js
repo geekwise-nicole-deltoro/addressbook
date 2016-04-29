1a2b4a41380af504a432277d7c32f1207fdea82b
Nicole Deltoro <geekwise.Nicole.deltoro@gmail.com>
2016-04-29 16:24:50 -0700

    edit
    got it too display finanlly

--- js/main.js
+++ js/main.js
@@ -86,46 +86,46 @@ document.addEventListener('DOMContentLoaded',function(event){
 };
     
   
-  var contact_array = [
-    alonzo,
-    j,
-    justton,
-    kao,
-    tiffany,
-    nicole,
-    veronica,
-    
-];
+//  var contact_array = [
+//    alonzo,
+//    j,
+//    justton,
+//    kao,
+//    tiffany,
+//    nicole,
+//    veronica,
+//    
+//];
 
-    for(i in contact_array){
-    
-        if(contact_array[i].toString(.match(/w/)===letter_container.textContent.toString().match(/w/));
-        
-        var full_name_container = document.createElement('p');
-        full_name_container.setAttribute('id', 'full_name_container_'+i);
-        full_name_container.textContent = contact_array[i].first_name + '' + contact.array[i].last_name;
-        console.log(full_name_container);
-        
-        
-                    abc.appendChild(full_name_container);
-                    
-                    
-                    
-                    if(
-                        document.getElementById('full_name_container_'' + i)).textContent[0] === document.getElementById('abc) + i).textContent[0]
-                            
-                            
-                            ){
-                            
-                            
-                                document.getElementById('abc' + i).appendChild(document.getElementById('full_name_container_' + i));
-                                
-                            }
-                    
-                    
-                };
-        
-          };
+//    for(i in contact_array){
+//    
+//        if(contact_array[i].toString(.match(/w/)===letter_container.textContent.toString().match(/w/));
+//        
+//        var full_name_container = document.createElement('p');
+//        full_name_container.setAttribute('id', 'full_name_container_'+i);
+//        full_name_container.textContent = contact_array[i].first_name + '' + contact.array[i].last_name;
+//        console.log(full_name_container);
+//        
+//        
+//                    abc.appendChild(full_name_container);
+//                    
+//                    
+//                    
+//                    if(
+//                        document.getElementById('full_name_container_'' + i)).textContent[0] === document.getElementById('abc) + i).textContent[0]
+//                            
+//                            
+//                            ){
+//                            
+//                            
+//                                document.getElementById('abc' + i).appendChild(document.getElementById('full_name_container_' + i));
+//                                
+//                            }
+//                    
+//                    
+//                };
+//        
+//          };
                 
                 
     });
